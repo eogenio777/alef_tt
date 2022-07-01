27daa50d35814822aebf524a4f977a9b767a8629
evgenii-podruzhinskii <evg.pod.dm@gmail.com>
2022-07-01 07:36:18 +0300

    Change psql ending for heroku

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from bot import MyBot
 import os
+import re
 
 
 def main():
     engine_str = os.environ['DATABASE_URL']
+    if engine_str.startswith("postgres://"):
+        engine_str = engine_str.replace("postgres://", "postgresql://", 1)
     token = os.environ['TOKEN']
     bot = MyBot(token, engine_str)
     bot.run_bot()
